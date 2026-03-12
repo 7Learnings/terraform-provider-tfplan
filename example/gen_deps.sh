ff7953fbdc6504e44aa15ba8daef0d7c91830983
Martin Nowak <code@dawg.eu>
2026-03-12 12:43:12 +0100

    fix: cyclic dependency between CHANGED_STACKS and deps.d
    - resolved by secondary expansion
    - this approach indeed seems safer than conditionally generating
      cross-stack dependencies in the script because the deps.d file could
      easily be stale otherwise

--- example/gen_deps.sh
+++ example/gen_deps.sh
@@ -88,8 +88,9 @@ for stack in "${STACKS[@]}"; do
     mapfile -t upstreams < <(sed -nE "s|.*\bstack\s*=\s*\"([^\"]+)\"|\1|p" "${!deps[@]}" 2>/dev/null)
     for upstream in "${upstreams[@]}"; do
         # When running plan-changed/apply-changed, only depend on upstream if it's also in CHANGED_STACKS
-        echo "$stack/\$(ENV)/tfplan.json: \$(if \$(filter plan-changed apply-changed,\$(MAKECMDGOALS)),\$(if \$(filter $upstream,\$(CHANGED_STACKS)),$upstream/\$(ENV)/tfplan.json),$upstream/\$(ENV)/tfplan.json)"
-        echo "$stack/\$(ENV)/outputs.json: \$(if \$(filter plan-changed apply-changed,\$(MAKECMDGOALS)),\$(if \$(filter $upstream,\$(CHANGED_STACKS)),$upstream/\$(ENV)/outputs.json),$upstream/\$(ENV)/outputs.json)"
+        # Using .SECONDEXPANSION to defer evaluation of CHANGED_STACKS because it's calculation cyclically depends on the generated dependency file itself (DOWNSTREAMS_x)
+        echo "$stack/\$(ENV)/tfplan.json: \$(if \$(filter plan-changed apply-changed,\$(MAKECMDGOALS)),\$\$(if \$\$(filter $upstream,\$\$(CHANGED_STACKS)),$upstream/\$(ENV)/tfplan.json),$upstream/\$(ENV)/tfplan.json)"
+        echo "$stack/\$(ENV)/outputs.json: \$(if \$(filter plan-changed apply-changed,\$(MAKECMDGOALS)),\$\$(if \$\$(filter $upstream,\$\$(CHANGED_STACKS)),$upstream/\$(ENV)/outputs.json),$upstream/\$(ENV)/outputs.json)"
         echo "DOWNSTREAMS_$upstream += $stack"
     done
 
